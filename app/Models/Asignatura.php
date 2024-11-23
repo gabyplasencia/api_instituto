2e0ffa55dc2cf6c1bf0d60d0d12e3bb5391a4e3c
Gabriela Plasencia <gabyplasencia1995@gmail.com>
2024-11-23 19:10:42 +0000

    añadiendo modelos con krlove

---
+++ app/Models/Asignatura.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $nombre
+ * @property string $curso
+ * @property AsignaturaMatricula[] $asignaturaMatriculas
+ */
+class Asignatura extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = ['nombre', 'curso'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function asignaturaMatriculas()
+    {
+        return $this->hasMany('App\Models\AsignaturaMatricula', 'idasignatura');
+    }
+
+    public function matriculas() {
+        return $this->belongsToMany(
+            Matricula::class,
+            'asignatura_matricula',
+            'idasignatura',
+            'idmatricula',
+        );
+    }
+}
--- config/app.php
+++ config/app.php
@@ -191,6 +191,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
